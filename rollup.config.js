05687b1ba5df43a6ab8a10a1419bff537d8b954c
Alexandr Shipulin <aleksandr.sh@pyrobyte.ru>
2021-10-26 15:08:19 +0700

    change config rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,37 @@
 import {nodeResolve} from "@rollup/plugin-node-resolve";
 import {babel} from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
+import pkg from './package.json';
 
-export default {
-  input: './src/index.js',
-  output: {
-    file: './dist/index.js',
-    format: 'cjs'
+export default [
+  {
+    input: './src/index.js',
+    output: {
+      name: 'validatorPyrobyte',
+      file: pkg.browser,
+      format: 'umd'
+    },
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      babel({
+        babelHelpers: 'bundled',
+        exclude: ['node_modules/**']
+      })
+    ]
   },
-  plugins: [
-    commonjs(),
-    nodeResolve(),
-    babel({babelHelpers: 'bundled'})
-  ]
-};
+  {
+    input: './src/index.js',
+    output: [
+      {file: pkg.main, format: 'cjs'},
+      {file: pkg.module, format: 'es'},
+    ],
+    plugins: [
+      nodeResolve(),
+      babel({
+        babelHelpers: 'bundled',
+        exclude: ['node_modules/**']
+      })
+    ]
+  }
+];
