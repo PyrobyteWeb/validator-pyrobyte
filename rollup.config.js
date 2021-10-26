52fa287e20d685900dca337448a78a53e1ce48fb
Alexandr Shipulin <aleksandr.sh@pyrobyte.ru>
2021-10-26 14:27:26 +0700

    change rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
 import {nodeResolve} from "@rollup/plugin-node-resolve";
 import {babel} from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
   input: './src/index.js',
   output: {
     file: './dist/index.js',
-    format: 'esm'
+    format: 'cjs'
   },
   plugins: [
+    commonjs(),
     nodeResolve(),
     babel({babelHelpers: 'bundled'})
   ]
