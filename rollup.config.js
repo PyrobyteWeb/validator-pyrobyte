402b14debf4c5030b073374ed99937ef1ba568e4
Alexandr Shipulin <aleksandr.sh@pyrobyte.ru>
2021-10-27 11:08:01 +0700

    add minify code by terser

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import {nodeResolve} from "@rollup/plugin-node-resolve";
 import {babel} from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import pkg from './package.json';
+import {terser} from "rollup-plugin-terser";
 
 export default [
   {
@@ -17,14 +18,15 @@ export default [
       babel({
         babelHelpers: 'bundled',
         exclude: ['node_modules/**']
-      })
+      }),
+      terser()
     ]
   },
   {
     input: './src/index.js',
     output: [
-      {file: pkg.main, format: 'cjs'},
-      {file: pkg.module, format: 'es'},
+      {file: pkg.main, format: 'cjs', plugins: [terser({toplevel: true})]},
+      {file: pkg.module, format: 'es', plugins: [terser({module: true})]},
     ],
     plugins: [
       nodeResolve(),
