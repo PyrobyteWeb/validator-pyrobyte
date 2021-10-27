d2b8860ff7892dc33571b714b8fa011108158365
Alexandr Shipulin <aleksandr.sh@pyrobyte.ru>
2021-10-27 14:21:39 +0700

    убрал umd модули

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,9 @@
 import {nodeResolve} from "@rollup/plugin-node-resolve";
 import {babel} from "@rollup/plugin-babel";
-import commonjs from "@rollup/plugin-commonjs";
 import pkg from './package.json';
 import {terser} from "rollup-plugin-terser";
 
 export default [
-  {
-    input: './src/index.js',
-    output: {
-      name: 'validatorPyrobyte',
-      file: pkg.browser,
-      format: 'umd'
-    },
-    plugins: [
-      nodeResolve(),
-      commonjs(),
-      babel({
-        babelHelpers: 'runtime',
-        exclude: ['node_modules/**']
-      }),
-      terser()
-    ]
-  },
   {
     external: [/@babel\/runtime/],
     input: './src/index.js',
