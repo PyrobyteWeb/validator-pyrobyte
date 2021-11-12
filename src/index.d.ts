2a29250c1aecb41aa1d0cc63d601df3f40483cd2
Alexandr Shipulin <aleksandr.sh@pyrobyte.ru>
2021-11-12 14:55:00 +0700

    change return value method checkAll

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,29 +1,33 @@
 interface IResultValidation {
-  passed: boolean,
-  errors: string[],
+  passed: boolean;
+  errors: string[];
 }
 
 interface IRule {
-  [nameRule: string]: number | string | boolean,
+  [nameRule: string]: number | string | boolean;
 }
 
 interface IRules {
-  [nameRule: string]: IRule,
+  [nameRule: string]: IRule;
 }
 interface IRulesValidation {
   [nameRule: string]: {
-    handler(value: string): boolean,
-    errorText: string,
-  }
+    handler(value: string): boolean;
+    errorText: string;
+  };
 }
 
 declare class Validator {
-  constructor(rules: IRules, rulesValidation?: IRulesValidation)
+  constructor(rules: IRules, rulesValidation?: IRulesValidation);
   check(name: string, data: string): IResultValidation;
-  checkAll(data: string): IRulesValidation;
-  changeRule(name: string, handler: (value: string) => boolean, errorText: string): void;
+  checkAll(data: string): IResultValidation;
+  changeRule(
+    name: string,
+    handler: (value: string) => boolean,
+    errorText: string
+  ): void;
 }
 
 declare let RULES_VALIDATION: IRulesValidation;
 
-export {Validator, RULES_VALIDATION};
+export { Validator, RULES_VALIDATION };
